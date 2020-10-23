ecc00d06bf77da54de04f04612ae024ba230613f
WebMaster124 <management.bam.website@gmail.com>
2020-10-23 10:21:20 +0000

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    isLogin:false,
+    localItem:null,
+	serverUrl:"http://trinipossecarnival.com/",
+    imageUrl: "http://trinipossecarnival.com/images/banner/",
+    imageProduct: "http://trinipossecarnival.com/images",
+};
